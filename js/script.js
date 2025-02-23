40c97b02ea2e893994ba3e722d4a91a522445325
Artur <example.gmail.com>
2025-02-23 08:34:48 +0100

    Operators

--- js/script.js
+++ js/script.js
@@ -23,23 +23,80 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('How many movies did you already watch?', '');
+// const numberOfFilms = +prompt('How many movies did you already watch?', '');
 
-let personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
+// let personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
 
 
-const a = prompt('Latest watched film?', ''),
-	  b = prompt('How would you rate it?', ''),
-	  c = prompt('Latest watched film?', ''),
-	  d = prompt('How would you rate it', '');
+// const a = prompt('Latest watched film?', ''),
+// 	  b = prompt('How would you rate it?', ''),
+// 	  c = prompt('Latest watched film?', ''),
+// 	  d = prompt('How would you rate it', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+console.log( NaN || 2 || undefined ); //2
+
+
+console.log( NaN && 2 && undefined ); //nan
+
+
+console.log( 1 && 2 && 3 ); //3
+
+
+console.log( !1 && 2 || !3 ); //false
+
+
+console.log( 25 || null && !3 ); //25
+
+
+console.log( NaN || null || !3 || undefined || 5); //5
+
+
+console.log( NaN || null && !3 && undefined || 5); //5
+
+
+console.log( 5 === 5 && 3 > 1 || 5); //true
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+let hamburger;
+const fries = NaN;
+const cola = 0;
+const nuggets = 2;
+
+
+if (hamburger && cola || fries === 3 && nuggets) {
+   console.log('Done!')
+}
+
+console.log(hamburger && cola || fries === 3 && nuggets);
