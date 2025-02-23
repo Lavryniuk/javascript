20d909dbaba0df311312ad12e4900b06e8733ea4
Artur <example.gmail.com>
2025-02-23 20:02:50 +0100

    Some exercises with loops

--- js/script.js
+++ js/script.js
@@ -203,57 +203,162 @@
 
 
 
-// Место для первой задачи
-function firstTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const arr = [3, 5, 8, 16, 20, 23, 50];
-    const result = [];
+// // Место для первой задачи
+// function firstTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
 
-    // Пишем решение вот тут
-    for (let i = 0; i < arr.length; i++) {
-		result[i] = arr[i];
-	}
-	console.log(result);
+//     // Пишем решение вот тут
+//     for (let i = 0; i < arr.length; i++) {
+// 		result[i] = arr[i];
+// 	}
+// 	console.log(result);
     
-    // Не трогаем
-    return result;
-}
+//     // Не трогаем
+//     return result;
+// }
 
-// Место для второй задачи
-function secondTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
+// // Место для второй задачи
+// function secondTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
 
-    // Пишем решение вот тут
+//     // Пишем решение вот тут
     
-	for (let i = 0; i < data.length; i++) {
-		if (typeof(data[i]) === 'number') {
-			data[i] = data[i] * 2;
-		} else if (typeof(data[i]) === 'string') {
-			data[i] = `${data[i]} - done`;
-		}
-		console.log(data);
-	}
+// 	for (let i = 0; i < data.length; i++) {
+// 		if (typeof(data[i]) === 'number') {
+// 			data[i] = data[i] * 2;
+// 		} else if (typeof(data[i]) === 'string') {
+// 			data[i] = `${data[i]} - done`;
+// 		}
+// 		console.log(data);
+// 	}
     
-    // Не трогаем
-    return data;
-}
+//     // Не трогаем
+//     return data;
+// }
 
-// Место для третьей задачи
-function thirdTask() {
-    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
-    const data = [5, 10, 'Shopping', 20, 'Homework'];
-    const result = [];
+// // Место для третьей задачи
+// function thirdTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     const result = [];
 
-    // Пишем решение вот тут
+//     // Пишем решение вот тут
     
-    for (let i = 1; i <= data.length; i++) {
-        result[i - 1] = data[data.length - i]
-    }
-	console.log(result);
-    // Не трогаем
-    return result;
-}
+//     for (let i = 1; i <= data.length; i++) {
+//         result[i - 1] = data[data.length - i]
+//     }
+
+//     // //for (let i = 0; i <= data.length; i++) {
+//     //     result[i] = data[data.length - 1 - i]
+//     // }
+    
+// 	console.log(result);
+//     // Не трогаем
+//     return result;
+// }
 
 
-//
+// //
+
+// another triangle
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+// console.log(result);
+
+// trening of cycles
+
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {            
+//     result += '*';
+//     }
+    
+//     result += "\n";
+// }
+// console.log(result);
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 1; i <= lines; i++) {
+//     for (let j = 0; j < i; j++) {
+//         result += '*';
+//     }
+//     result += '\n';
+// }
+
+// for (let i = lines - 1; i >= 1; i--) {
+//     for (let j = 0; j < i; j++) {
+//         result += '*';
+//     }
+//     result += '\n';
+// }
+// console.log(result);
+// *
+// **
+// ***
+// ****
+// *****
+// ****
+// ***
+// **
+// *
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 1; i <= lines; i++) {
+//     for (let j = 0; j < i; j++) {
+//         result += i;
+//     }
+//     result += '\n';
+// }
+// console.log(result);
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 1; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += ' ';
+//     }
+//     for (let k = 0; k < i * 2 - 1; k++) {
+//         result += i;
+//     }
+//     result += '\n';
+// }
+// console.log(result);
+//     1
+//    222
+//   33333
+//  4444444
+// 555555555
